66757eff087b02462c8c8fbe48aefa43d434acb2
Willian Campos <williancampos1995@gmail.com>
2020-07-07 00:18:27 -0300

    Done first two screens and navbar

---
+++ src/components/Activities/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Feather, MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
+
+import {
+  Container,
+  Header,
+  Title,
+  Card,
+  CardHeader,
+  Avatar,
+  Description,
+  Bold,
+  CardBody,
+  UserName,
+  CardFooter,
+  Details,
+  Value,
+  Divider,
+  Date,
+  Actions,
+  Option,
+  OptionLabel,
+} from './styles';
+
+import avatar from '../../images/avatar.png';
+
+export default function Activities() {
+  return (
+    <Container>
+      <Header>
+        <Title>Atividades</Title>
+      </Header>
+
+      <Card>
+        <CardHeader>
+          <Avatar source={avatar} />
+          <Description>
+            <Bold>Você</Bold> pagou a <Bold>@will.camposs</Bold>
+          </Description>
+        </CardHeader>
+
+        <CardBody>
+          <UserName>Willian Campos</UserName>
+        </CardBody>
+
+        <CardFooter>
+          <Details>
+            <Value>R$ 1000,00</Value>
+
+            <Divider />
+
+            <Feather name="lock" color="#fff" size={14} />
+            <Date>há 2 anos</Date>
+          </Details>
+
+          <Actions>
+            <Option>
+              <MaterialCommunityIcons name="comment-outline" size={14} color="#fff" />
+              <OptionLabel>0</OptionLabel>
+            </Option>
+
+            <Option>
+              <AntDesign name="hearto" size={14} color="#fff" />
+              <OptionLabel>0</OptionLabel>
+            </Option>
+          </Actions>
+        </CardFooter>
+      </Card>
+    </Container>
+  );
+}
---
+++ src/screens/Home/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
+
+import { Wrapper, Container, Header, BalanceContainer, BalanceTitle, Balance } from './styles';
+
+import Suggestions from '../../components/Suggestions';
+import Activities from '../../components/Activities';
+import Tips from '../../components/Tips';
+import Banner from '../../components/Banner';
+
+export default function Home() {
+  return (
+    <Wrapper>
+      <Container>
+        <Header>
+          <MaterialCommunityIcons name="qrcode-scan" size={30} color="#10c86e" />
+
+          <BalanceContainer>
+            <BalanceTitle>Meu saldo</BalanceTitle>
+            <Balance>R$ 0,00</Balance>
+          </BalanceContainer>
+
+          <AntDesign name="gift" size={30} color="#10c86e" />
+        </Header>
+
+        <Suggestions />
+        <Activities />
+        <Tips />
+        <Banner />      
+      </Container>
+    </Wrapper>
+  );
+}
